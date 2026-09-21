41fff01efd71b5f2b9de8c8f1ac70b8831e7de7b
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-21 09:29:46 -0400

    feat(quests): edit poker win, winnings and level objectives

--- Intersect.Editor/Forms/Editors/Quest/Quest_TaskEditor.cs
+++ Intersect.Editor/Forms/Editors/Quest/Quest_TaskEditor.cs
@@ -5,6 +5,7 @@ using Intersect.Enums;
 using Intersect.Framework.Core.GameObjects.Items;
 using Intersect.Framework.Core.GameObjects.NPCs;
 using Intersect.Framework.Core.GameObjects.Quests;
+using Intersect.Framework.Core.MiniGames;
 using Intersect.GameObjects;
 using Microsoft.Extensions.Logging;
 
@@ -23,6 +24,13 @@ public partial class QuestTaskEditor : UserControl
 
     private QuestTaskDescriptor mMyTask;
 
+    private readonly List<Guid> mMiniGameCurrencyIds = new();
+    private DarkUI.Controls.DarkGroupBox mGrpMiniGame = null!;
+    private DarkUI.Controls.DarkComboBox mCmbMiniGame = null!;
+    private DarkUI.Controls.DarkComboBox mCmbMiniGameCurrency = null!;
+    private DarkUI.Controls.DarkNumericUpDown mNudMiniGameAmount = null!;
+    private Label mLblMiniGameCurrency = null!;
+
     public QuestTaskEditor(QuestDescriptor refQuest, QuestTaskDescriptor refTask)
     {
         if (refQuest == null)
@@ -36,6 +44,7 @@ public partial class QuestTaskEditor : UserControl
         }
 
         InitializeComponent();
+        InitializeMiniGameControls();
         mMyTask = refTask;
         mMyQuest = refQuest;
 
@@ -63,6 +72,17 @@ public partial class QuestTaskEditor : UserControl
                 nudNpcQuantity.Value = mMyTask?.Quantity ?? 0;
 
                 break;
+            case 3:
+            case 4:
+            case 5:
+                mCmbMiniGame.SelectedIndex = 0;
+                mNudMiniGameAmount.Value = Math.Clamp(mMyTask?.Quantity ?? 1, 1, int.MaxValue);
+                if (cmbTaskType.SelectedIndex == 4)
+                {
+                    var currencyIndex = mMiniGameCurrencyIds.IndexOf(mMyTask?.TargetId ?? Guid.Empty);
+                    mCmbMiniGameCurrency.SelectedIndex = currencyIndex >= 0 ? currencyIndex : 0;
+                }
+                break;
         }
     }
 
@@ -76,6 +96,10 @@ public partial class QuestTaskEditor : UserControl
         {
             cmbTaskType.Items.Add(Strings.TaskEditor.types[i]);
         }
+        while (cmbTaskType.Items.Count < 3) cmbTaskType.Items.Add("Task");
+        cmbTaskType.Items.Add("Mini-game: Win games");
+        cmbTaskType.Items.Add("Mini-game: Win amount");
+        cmbTaskType.Items.Add("Mini-game: Reach level");
 
         lblDesc.Text = Strings.TaskEditor.desc;
 
@@ -98,6 +122,7 @@ public partial class QuestTaskEditor : UserControl
     {
         grpGatherItems.Hide();
         grpKillNpcs.Hide();
+        mGrpMiniGame.Hide();
         switch (cmbTaskType.SelectedIndex)
         {
             case 0: //Event Driven
@@ -126,6 +151,16 @@ public partial class QuestTaskEditor : UserControl
                 nudNpcQuantity.Value = 1;
 
                 break;
+            case 3:
+            case 4:
+            case 5:
+                mGrpMiniGame.Show();
+                mLblMiniGameCurrency.Visible = cmbTaskType.SelectedIndex == 4;
+                mCmbMiniGameCurrency.Visible = cmbTaskType.SelectedIndex == 4;
+                if (mCmbMiniGame.SelectedIndex < 0) mCmbMiniGame.SelectedIndex = 0;
+                if (mCmbMiniGameCurrency.SelectedIndex < 0 && mCmbMiniGameCurrency.Items.Count > 0) mCmbMiniGameCurrency.SelectedIndex = 0;
+                if (mNudMiniGameAmount.Value < 1) mNudMiniGameAmount.Value = 1;
+                break;
         }
     }
 
@@ -150,9 +185,61 @@ public partial class QuestTaskEditor : UserControl
                 mMyTask.Quantity = (int) nudNpcQuantity.Value;
 
                 break;
+            case QuestObjective.MiniGameWins:
+            case QuestObjective.MiniGameWinnings:
+            case QuestObjective.MiniGameLevel:
+                mMyTask.MiniGameKey = mCmbMiniGame.SelectedIndex == 0 ? MiniGameProgression.Poker : MiniGameProgression.Poker;
+                mMyTask.Quantity = (int)mNudMiniGameAmount.Value;
+                mMyTask.TargetId = mMyTask.Objective == QuestObjective.MiniGameWinnings && mCmbMiniGameCurrency.SelectedIndex >= 0
+                    ? mMiniGameCurrencyIds[mCmbMiniGameCurrency.SelectedIndex]
+                    : Guid.Empty;
+                break;
         }
 
         ParentForm.Close();
+    }
+
+    private void InitializeMiniGameControls()
+    {
+        mGrpMiniGame = new DarkUI.Controls.DarkGroupBox
+        {
+            Text = "Mini-game objective", Location = new System.Drawing.Point(10, 110),
+            Size = new System.Drawing.Size(236, 83), Visible = false,
+            BackColor = System.Drawing.Color.FromArgb(45, 45, 48),
+            ForeColor = System.Drawing.Color.Gainsboro,
+        };
+        var lblGame = new Label { Text = "Game:", AutoSize = true, Location = new System.Drawing.Point(7, 23) };
+        mCmbMiniGame = new DarkUI.Controls.DarkComboBox
+        {
+            Location = new System.Drawing.Point(104, 19), Size = new System.Drawing.Size(116, 21),
+            DropDownStyle = ComboBoxStyle.DropDownList
+        };
+        mCmbMiniGame.Items.Add("Poker"); mCmbMiniGame.SelectedIndex = 0;
+
+        var lblAmount = new Label { Text = "Target:", AutoSize = true, Location = new System.Drawing.Point(7, 55) };
+        mNudMiniGameAmount = new DarkUI.Controls.DarkNumericUpDown
+        {
+            Location = new System.Drawing.Point(104, 52), Size = new System.Drawing.Size(116, 20),
+            Minimum = 1, Maximum = int.MaxValue, Value = 1
+        };
+        mLblMiniGameCurrency = new Label { Text = "Currency:", AutoSize = true, Location = new System.Drawing.Point(7, 23), Visible = false };
+        mCmbMiniGameCurrency = new DarkUI.Controls.DarkComboBox
+        {
+            Location = new System.Drawing.Point(104, 19), Size = new System.Drawing.Size(116, 21),
+            DropDownStyle = ComboBoxStyle.DropDownList, Visible = false
+        };
+        foreach (var item in MiniGameCurrency.CompatibleItems(ItemDescriptor.Lookup.Values.OfType<ItemDescriptor>()))
+        {
+            mMiniGameCurrencyIds.Add(item.Id);
+            mCmbMiniGameCurrency.Items.Add(item.Name);
+        }
+        if (mCmbMiniGameCurrency.Items.Count > 0) mCmbMiniGameCurrency.SelectedIndex = 0;
+
+        mGrpMiniGame.Controls.Add(lblGame); mGrpMiniGame.Controls.Add(mCmbMiniGame);
+        mGrpMiniGame.Controls.Add(lblAmount); mGrpMiniGame.Controls.Add(mNudMiniGameAmount);
+        mGrpMiniGame.Controls.Add(mLblMiniGameCurrency); mGrpMiniGame.Controls.Add(mCmbMiniGameCurrency);
+        grpEditor.Controls.Add(mGrpMiniGame);
+        mGrpMiniGame.BringToFront();
     }
 
     private void btnCancel_Click(object sender, EventArgs e)
