848f843357db11d5d673a3e394e4814066f29cc2
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-21 16:01:22 -0400

    feat(blackjack): integrate with current mini-game runtime

--- Framework/Intersect.Framework.Core/MiniGames/Configuration/MiniGameCatalog.cs
+++ Framework/Intersect.Framework.Core/MiniGames/Configuration/MiniGameCatalog.cs
@@ -18,6 +18,7 @@ public static class MiniGameCatalog
     private static readonly MiniGameDefinition[] Definitions =
     [
         new(MiniGameType.Poker, "Poker - Texas hold'em", 2, 6, "poker-1"),
+        new(MiniGameType.Blackjack, "Blackjack - versus dealer", 2, 6, "blackjack-1"),
     ];
 
     public static IReadOnlyList<MiniGameDefinition> All => Definitions;
