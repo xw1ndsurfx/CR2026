71296d2cdd94422a88f08d936c3c0150e1b37342
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-21 22:43:23 -0400

    Fix world map GameContentManager namespace

--- Intersect.Client.Core/Interface/Game/WorldMapWindow.cs
+++ Intersect.Client.Core/Interface/Game/WorldMapWindow.cs
@@ -1,5 +1,6 @@
 using Intersect.Client.Core;
 using Intersect.Client.Framework.Content;
+using Intersect.Client.Framework.File_Management;
 using Intersect.Client.Framework.GenericClasses;
 using Intersect.Client.Framework.Graphics;
 using Intersect.Client.Framework.Gwen;
