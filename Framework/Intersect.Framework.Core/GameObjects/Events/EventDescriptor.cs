1c420289911d61a6ee8d8b13f9d9703e714a0f60
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-22 00:04:09 -0400

    Persist world map event markers and reveal partial map previews

--- Framework/Intersect.Framework.Core/GameObjects/Events/EventDescriptor.cs
+++ Framework/Intersect.Framework.Core/GameObjects/Events/EventDescriptor.cs
@@ -73,6 +73,8 @@ public partial class EventDescriptor : DatabaseObject<EventDescriptor>, IFoldera
 
     public bool Global { get; set; }
 
+    public bool ShowAnimationOnWorldMap { get; set; }
+
     [JsonIgnore]
     [Column("Pages")]
     public string PagesJson
