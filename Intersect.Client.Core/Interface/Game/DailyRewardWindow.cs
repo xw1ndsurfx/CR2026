433eda70124deae472e85f1f0b0db43d6dcd817a
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-22 11:00:45 -0400

    fix(daily-rewards): import GameContentManager namespace

--- Intersect.Client.Core/Interface/Game/DailyRewardWindow.cs
+++ Intersect.Client.Core/Interface/Game/DailyRewardWindow.cs
@@ -1,5 +1,6 @@
 using Intersect.Client.Core;
 using Intersect.Client.Framework.Content;
+using Intersect.Client.Framework.File_Management;
 using Intersect.Client.Framework.Gwen;
 using Intersect.Client.Framework.Gwen.Control;
 using Intersect.Client.General;
