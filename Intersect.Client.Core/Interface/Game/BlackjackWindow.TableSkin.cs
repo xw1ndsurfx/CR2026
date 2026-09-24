70ae1762540a99b48b5ec175cff10caf5645ab64
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-24 14:49:13 -0400

    fix(blackjack-ui): use long timer values

--- Intersect.Client.Core/Interface/Game/BlackjackWindow.TableSkin.cs
+++ Intersect.Client.Core/Interface/Game/BlackjackWindow.TableSkin.cs
@@ -144,7 +144,7 @@ internal sealed partial class BlackjackWindow
         UpdateTableSkinTimer(timed && seconds > 0, seconds);
     }
 
-    private void UpdateTableSkinTimer(bool visible, int seconds)
+    private void UpdateTableSkinTimer(bool visible, long seconds)
     {
         SetTableSkinVisible(_timerSkin, visible);
 
