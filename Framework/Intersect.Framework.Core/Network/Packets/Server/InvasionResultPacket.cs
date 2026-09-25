d16640e4db9d513b43d5df0c8f2e4d14ef9353a5
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-25 18:16:00 -0400

    feat(invasions): add invasion result reward packet

---
+++ Framework/Intersect.Framework.Core/Network/Packets/Server/InvasionResultPacket.cs
@@ -0,0 +1,18 @@
+using MessagePack;
+
+namespace Intersect.Network.Packets.Server;
+
+[MessagePackObject]
+public partial class InvasionResultPacket : IntersectPacket
+{
+    public InvasionResultPacket() { }
+
+    [Key(0)] public Guid InvasionId { get; set; }
+    [Key(1)] public string Name { get; set; } = string.Empty;
+    [Key(2)] public bool Victory { get; set; }
+    [Key(3)] public long ExperienceAwarded { get; set; }
+    [Key(4)] public int WavesCompleted { get; set; }
+    [Key(5)] public int WaveCount { get; set; }
+    [Key(6)] public int ObjectiveHealthRemaining { get; set; }
+    [Key(7)] public int ParticipantCount { get; set; }
+}
