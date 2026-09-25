9582fc0c7c0c479d384e11e0ddc49015266a7d35
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-25 18:15:56 -0400

    feat(invasions): add configuration response packet

---
+++ Framework/Intersect.Framework.Core/Network/Packets/Server/InvasionConfigurationPacket.cs
@@ -0,0 +1,21 @@
+using MessagePack;
+
+namespace Intersect.Network.Packets.Server;
+
+[MessagePackObject]
+public partial class InvasionConfigurationPacket : IntersectPacket
+{
+    public InvasionConfigurationPacket() { }
+
+    public InvasionConfigurationPacket(string configurationJson, bool openEditor)
+    {
+        ConfigurationJson = configurationJson;
+        OpenEditor = openEditor;
+    }
+
+    [Key(0)]
+    public string ConfigurationJson { get; set; } = string.Empty;
+
+    [Key(1)]
+    public bool OpenEditor { get; set; }
+}
