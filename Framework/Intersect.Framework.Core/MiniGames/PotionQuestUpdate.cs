c0649a1970d1cd2beae5cbb8c5dbfcd1fe6189e9
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-25 03:17:38 -0400

    feat(potions): expose chain board and recipe score to quests

--- Framework/Intersect.Framework.Core/MiniGames/PotionQuestUpdate.cs
+++ Framework/Intersect.Framework.Core/MiniGames/PotionQuestUpdate.cs
@@ -4,4 +4,7 @@ public readonly record struct PotionQuestUpdate(
     bool BrewedRecipe,
     Guid RecipeId,
     int AlchemyLevel,
-    int ScoreGained);
+    int ScoreGained,
+    int ChainAchieved = 0,
+    int OccupiedCells = 0,
+    int RecipeScore = 0);
