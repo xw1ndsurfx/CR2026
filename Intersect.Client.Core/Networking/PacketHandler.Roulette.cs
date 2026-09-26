9692cc5edf0cb6b0c4fea0fb2ebfe3938eb62a9a
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-26 06:09:11 -0400

    Add roulette network handlers

---
+++ Intersect.Client.Core/Networking/PacketHandler.Roulette.cs
@@ -0,0 +1,14 @@
+using Intersect.Network;
+using Intersect.Network.Packets.Server;
+using ClientInterface = Intersect.Client.Interface.Interface;
+
+namespace Intersect.Client.Networking;
+
+internal sealed partial class PacketHandler
+{
+    public void HandlePacket(IPacketSender sender, RouletteStatePacket packet)
+    {
+        if (packet.IsValid)
+            ClientInterface.GameUi?.QueueRouletteState(packet);
+    }
+}
