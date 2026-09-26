c824a714ca34deb8c5648e158076791022590069
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-26 06:08:02 -0400

    Add roulette progression key

--- Framework/Intersect.Framework.Core/MiniGames/MiniGameProgression.cs
+++ Framework/Intersect.Framework.Core/MiniGames/MiniGameProgression.cs
@@ -6,6 +6,7 @@ public static class MiniGameProgression
     public const string Poker = "poker";
     public const string Blackjack = "blackjack";
     public const string Potions = "potions";
+    public const string Roulette = "roulette";
     public const int ExperiencePerWin = 25;
     public const int MaximumLevel = 25;
     public const long MaximumExperience = 30_000;
