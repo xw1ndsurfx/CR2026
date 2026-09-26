99f3769ce809fe8d3de679f9a1b424a34bba80e5
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-26 01:48:53 -0400

    feat(invasions): apply live world atmosphere on client

--- Intersect.Client.Core/Networking/PacketHandler.Invasions.cs
+++ Intersect.Client.Core/Networking/PacketHandler.Invasions.cs
@@ -1,4 +1,5 @@
 using Intersect.Network;
+using Intersect.Client.WorldEvents.Invasions;
 using Intersect.Network.Packets.WorldEvents;
 
 namespace Intersect.Client.Networking;
@@ -7,6 +8,8 @@ internal sealed partial class PacketHandler
 {
     public void HandlePacket(IPacketSender packetSender, InvasionStatusPacket packet)
     {
+        InvasionEnvironmentManager.ApplyStatus(packet);
+
         if (!Intersect.Client.Interface.Interface.HasInGameUI) return;
         Intersect.Client.Interface.Interface.GameUi.UpdateInvasionStatus(packet);
     }
