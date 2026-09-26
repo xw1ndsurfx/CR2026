1aaf068888902cd475f4364d6b5e8ff62b217154
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-25 21:05:31 -0400

    fix(protocol): use stable world-event packet namespace

--- Intersect.Client.Core/Interface/Game/InvasionStatusWindow.cs
+++ Intersect.Client.Core/Interface/Game/InvasionStatusWindow.cs
@@ -1,7 +1,7 @@
 using Intersect.Client.Framework.File_Management;
 using Intersect.Client.Framework.Gwen;
 using Intersect.Client.Framework.Gwen.Control;
-using Intersect.Network.Packets.Server;
+using Intersect.Network.Packets.WorldEvents;
 using Rectangle = Intersect.Client.Framework.GenericClasses.Rectangle;
 using SkinBase = Intersect.Client.Framework.Gwen.Skin.Base;
 
