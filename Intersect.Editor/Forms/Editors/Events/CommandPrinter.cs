7c5e7a74ad70fbf1e65bd791b9edb4b6dcbe83b2
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2026-09-26 04:01:02 -0400

    Print profession event commands

--- Intersect.Editor/Forms/Editors/Events/CommandPrinter.cs
+++ Intersect.Editor/Forms/Editors/Events/CommandPrinter.cs
@@ -11,6 +11,7 @@ using Intersect.Framework.Core.GameObjects.Maps.MapList;
 using Intersect.Framework.Core.GameObjects.NPCs;
 using Intersect.Framework.Core.GameObjects.PlayerClass;
 using Intersect.Framework.Core.GameObjects.Variables;
+using Intersect.Framework.Core.Professions;
 using Intersect.GameObjects;
 using Microsoft.Extensions.Logging;
 using VariableMod = Intersect.Framework.Core.GameObjects.Events.VariableMod;
@@ -829,6 +830,19 @@ public static partial class CommandPrinter
         {
             return Strings.EventCommandList.giveexp.ToString(command.Exp);
         }
+    }
+
+    private static string GetCommandText(ModifyProfessionCommand command, MapInstance map)
+    {
+        var profession = ProfessionConfiguration.Instance.Find(command.ProfessionId);
+        var name = profession?.Name ?? "Unknown Profession";
+        var value = command.Action is ProfessionModification.AddExperience
+            or ProfessionModification.SetExperience
+            or ProfessionModification.SetLevel
+            ? $" {command.Value:N0}"
+            : string.Empty;
+
+        return $"Profession: {command.Action} {name}{value}";
     }
 
     private static string GetCommandText(ChangeLevelCommand command, MapInstance map)
